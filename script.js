b0ebb969fbed9bc11a852b2f0a0a6d66c178f866
rfdllh291105-crypto <rfdllh291105@gmail.com>
2025-12-18 13:00:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 <script>
-  function kirimKeWA(event) {
+  function kirimKeWA(event) 
     event.preventDefault();
 
     const nama = document.getElementById("nama").value;
     const pesan = document.getElementById("pesan").value;
 
-    const nomorWA = "6288210714192"; // nomor WA lu
+    const nomorWA = "+6288210714192"; // nomor WA lu
 
     const text = `Halo Rizal,
 
@@ -15,5 +15,5 @@ ${pesan}`;
     const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(text)}`;
 
     window.open(url, "_blank");
-  }
+    
 </script>
