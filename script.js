c5126a3587b15f79b0f37bf360d537b478bd1dcc
rfdllh291105-crypto <rfdllh291105@gmail.com>
2025-12-18 12:27:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function kirimKeWA(event) {
+  event.preventDefault();
+
+  const nama = document.getElementById('nama').value;
+  const pesan = document.getElementById('pesan').value;
+
+  const nomorWA = '6288210714192'; // tanpa +
+
+  const text = `Halo Rizal,
+
+Saya ${nama} ingin memberikan masukan:
+${pesan}`;
+
+  const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(text)}`;
+
+  window.open(url, '_blank');
+}
